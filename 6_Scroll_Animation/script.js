4ad543c111efc80fc117c11201bd351e0308bc3a
张欧文 <1534661514@qq.com>
2023-01-22 17:45:41 +0800

    getBounding Client

--- 6_Scroll_Animation/script.js
+++ 6_Scroll_Animation/script.js
@@ -1,19 +1,12 @@
 "use strict";
-const bg = document.querySelector(".bg");
-const loadingText = document.querySelector(".loading-text");
+const boxes = document.querySelectorAll(".box");
 
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-};
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+  });
+}
 
-const blurring = function () {
-  load++;
-  if (load > 99) {
-    clearInterval(timer);
-  }
-  loadingText.innerText = `${load}%`;
-  loadingText.style.opacity = scale(load, 0, 100, 1, 0);
-  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
-};
-let load = 0;
-let timer = setInterval(blurring, 30);
+window.addEventListener("scroll", checkBoxes);
+checkBoxes();

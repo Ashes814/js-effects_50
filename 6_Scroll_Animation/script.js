defc6751afe18d0939db6a74bfa2673932327dbe
张欧文 <1534661514@qq.com>
2023-01-29 12:54:18 +0800

    finish 6_scroll_Animation

--- 6_Scroll_Animation/script.js
+++ 6_Scroll_Animation/script.js
@@ -2,15 +2,16 @@
 const boxes = document.querySelectorAll(".box");
 
 function checkBoxes() {
-  const triggerBottom = (window.innerHeight / 5) * 40;
+  const triggerBottom = (window.innerHeight / 5) * 4;
   boxes.forEach((box) => {
     const boxTop = box.getBoundingClientRect().top;
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
   });
 }
 
 window.addEventListener("scroll", checkBoxes);
-checkBoxes();
-checkBoxes();
-checkBoxes();
-checkBoxes();
 checkBoxes();

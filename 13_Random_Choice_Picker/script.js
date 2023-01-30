4dd63b74baa6edd9819089dd0ca2de4775cdcf13
张欧文 <1534661514@qq.com>
2023-01-30 13:28:44 +0800

    finish 13 random choice picker

---
+++ 13_Random_Choice_Picker/script.js
@@ -0,0 +1,61 @@
+"use strict";
+const tagsEl = document.getElementById("tags");
+const textarea = document.getElementById("textarea");
+
+textarea.focus();
+
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+
+    randomSelect();
+  }
+});
+
+function createTags(input) {
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+  tagsEl.innerHTML = tags
+    .map((tag) => {
+      return `<span class='tag'>${tag}</span>`;
+    })
+    .join("");
+}
+
+function randomSelect() {
+  const times = 3;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+    highlightTag(randomTag);
+    setTimeout(() => {
+      removeHighlightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+    setTimeout(() => {
+      highlightTag(pickRandomTag());
+    }, 100);
+  }, times * 1000);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add("highlight");
+}
+
+function removeHighlightTag(tag) {
+  tag.classList.remove("highlight");
+}
